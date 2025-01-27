7fdbab3cd2e1fe998e5408565e7cf71e0222758c
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-01-27 22:15:48 +0500

    Hamamd Updated this today

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId=12345
+let accountEmail="hammad@gmail.com"
+var accountPassword="345123"
+accountCity="jaipur"
+let emailState;
+
+// accountId=23
+accountEmail="hc@gmail.com"
+accountPassword="786967"
+accountCity="ryk"
+
+/* Not to use Var 
+because issue comes of the scope {This curly braces is also called scope in the javaScript} */
+
+
+
+console.log(accountEmail);
+
+console.table([accountEmail,accountPassword,accountId,accountCity,emailState])
+
