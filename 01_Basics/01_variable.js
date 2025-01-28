460d66bb433a12c8d60da6d150843c81f48b0b9e
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-01-28 17:54:19 +0500

    Hammad Updated the code

--- 01_Basics/01_variable.js
+++ 01_Basics/01_variable.js
@@ -1,20 +1,33 @@
-const accountId=12345
-let accountEmail="hammad@gmail.com"
-var accountPassword="345123"
-accountCity="jaipur"
-let emailState;
+// const accountId=12345
+// let accountEmail="hammad@gmail.com"
+// var accountPassword="345123"
+// accountCity="jaipur"
+// let emailState;
 
 // accountId=23
-accountEmail="hc@gmail.com"
-accountPassword="786967"
-accountCity="ryk"
+// accountEmail="hc@gmail.com"
+// accountPassword="786967"
+// accountCity="ryk"
 
 /* Not to use Var 
 because issue comes of the scope {This curly braces is also called scope in the javaScript} */
 
 
 
-console.log(accountEmail);
+// console.log(accountEmail);
 
-console.table([accountEmail,accountPassword,accountId,accountCity,emailState])
+// console.table([accountEmail,accountPassword,accountId,accountCity,emailState])
 
+
+const hammadId=123
+let hammadEmail="hammad@gmail.com"
+var hammadPassword="245673"
+hammadCity="RYK"
+
+hammadEmail="choudhary@gmail.com"
+hammadPassword="478363"
+hammadCity="Lahore"
+
+
+console.table([hammadEmail,hammadId,hammadPassword,hammadCity]);
+ 
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+// alert(3+3)
+
+let name ="hammad"
+let age=22
+let isLoggedIn=false
+let hammad;
+
+
+// number 2 power of 53 
+// bigint : if value is too large 
+//string :""
+//boolean:true or false 
+//null :empty value and standalone 
+//undefined: variable is declared but not assign
+//symbol: uniuqeness
+
+
+// object
+
+console.log(typeof undefined ); // this is the undefined type 
+console.log(typeof null);//Null is the object 
+
