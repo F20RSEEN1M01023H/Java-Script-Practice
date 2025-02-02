0c73cd90cf2444311b7ecfcbcba728ada63677c5
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-02-02 15:58:14 +0500

    Data types conversion operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,2 +1,36 @@
-let score =45
+let score ="45abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// when i want to convert the string data to number then we do this 
+// let hammadScore=Number(score);
+// console.log(typeof hammadScore);
+// console.log(hammadScore);
+
+// When the mix string comes the number and the alphabets then that is not a number that is the NaN(not a number)
+// when the data store in the score is null then the output comes is the 0
+// 33 => 33 Number
+// "33"=>string 
+// "33abc" =>NaN
+// null =>0 
+//  true=>1 and false=>0
+
+// let IsloggedIn=0;
+// let booleanIsloggedIn=Boolean(IsloggedIn)
+// console.log(booleanIsloggedIn);
+
+
+
+// 0=>false and 1=>true 
+//"" =>false
+//"Hammad" =>true
+
+let someNumber="33";
+
+let someString=String(someNumber);
+console.log(typeof someString);
+console.log(someNumber);
+
+
 
