6028c95976c7a7b598389c3e854e89b8161e7a83
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-12-15 10:42:08 +0500

    logic building concepts

--- js logic build prac/index.js
+++ js logic build prac/index.js
@@ -30,9 +30,43 @@ button.addEventListener("click", () => {
   }
 });
 
+// function gets two numbers and give output
+
 function add(num1, num2) {
   const result = document.getElementById("output");
   const result2 = num1 + num2;
   result.textContent = `The sum is ${result2}`;
 }
 add(5, 10);
+
+// Check the eligibilty for voting
+
+const btn1 = document.getElementById("btn1");
+const ageDisplay = document.getElementById("age");
+const resultDisplay = document.getElementById("result");
+
+btn1.addEventListener("click", () => {
+  age = parseInt(prompt("Enter your age:"));
+  ageDisplay.textContent = age;
+  if (age < 0 || isNaN(age)) {
+    resultDisplay.textContent = "Please enter a valid age";
+    return;
+  }
+  if (age >= 18) {
+    resultDisplay.textContent = "You are eligible for voting";
+  } else {
+    resultDisplay.textContent = "You are not eligible for voting";
+  }
+});
+
+// loop for ( 1 to 5 )
+for (let i = 1; i <= 5; i++) {
+  console.log(`Iteration number: ${i}`);
+}
+
+// by using while loop
+let i = 1;
+while (i <= 5) {
+  console.log(`While Loop Iteration number: ${i}`);
+  i++;
+}
