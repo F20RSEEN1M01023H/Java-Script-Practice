58a70a02d9da40a412840a60bd548a9aad5b6d1b
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-12-16 20:46:12 +0500

    update

--- js logic build prac/index.js
+++ js logic build prac/index.js
@@ -181,3 +181,43 @@ function genrateTableReverse(num) {
   }
 }
 genrateTableReverse(5);
+
+// print even numbers from 1 to 20
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(`Even Number: ${i}`);
+  }
+}
+
+// print odd numbers from 1 to 15
+
+for (let i = 1; i <= 15; i++) {
+  if (i % 2 !== 0) {
+    console.log(`Odd Number: ${i}`);
+  }
+}
+
+// print numbers from 1 to 20 and if number is divisible by 3 print fizz else number
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
+
+// print numbers from 1 to 30 and if number is divisible by 5 print buzz,number is divisible by 3 print fizz and number is divisible by both 3 and 5 print fizzbuzz else number
+
+for (let i = 1; i <= 30; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
